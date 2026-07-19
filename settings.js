0a36dcd1f844a4d36e625922b7a055129ee0c0cf
usebot772-rgb <usebot772@gmail.com>
2026-07-19 14:09:44 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,7 +17,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=e534249adda44121' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=ede33a4457b8bd92' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
